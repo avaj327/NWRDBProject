b9bb8b5fb239b460c19928eaa9c93bf27e713840
tjohnson327 <35973904+tjohnson327@users.noreply.github.com>
2018-11-14 21:55:13 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -8,13 +8,9 @@ Created on Mon Nov  5 00:06:53 2018
 
 from flask import Flask, request, render_template, session, redirect
 import sqlite3
-from passlib.hash import sha256_crypt
 app = Flask(__name__)
 
 app.secret_key = "53Da__de39^^w32$5)*8"
-
-conn = sqlite3.connect('database.db')
-cur = conn.cursor()
 
 @app.route('/')
 def index():
@@ -27,50 +23,53 @@ def hello():
 @app.route('/user')
 def viewUser():
 	if ('username' in session):
-		return "Hello %s" % (session['username'])
+		user = session['user']
+		username = user[0]
+		password = user[1]
+		fact = user[2]
+		
+		return render_template('user.html', username=username,password=password,fact=fact)
 	else:
 		return 'Please sign in.'
-
-@app.route('/user/<USER>')
-def userPage(USER):
-	return "User: %s" % (USER)
 
 @app.route('/html')
 def html():
 	return render_template('test.html', name='Sean')
 
-@app.route('/testlogin', methods=["POST", "GET"])
-def testlogin():
+@app.route('/login', methods=["POST", "GET"])
+def login():
 	if (request.method=="POST"):
-		session['username'] = request.form['username']
+		conn = sqlite3.connect('database.db')
+		cur = conn.cursor()
+		username = request.form['username']
+		password = request.form['password']
+		user = None
+		for row in cur.execute("SELECT * FROM users"):
+			if row[0] == username:
+				user = row
+				
+		if user == None:
+			return 'Invalid Username'
+			
+		if password == user[1]:
+			session['user'] = user
+		else:
+			return 'Invalid Password'
+			
 		return redirect('/user')
 	else:
 		return render_template("form.html")
 	
+
 @app.route('/makeuser', methods=["POST", "GET"])
 def makeuser():
 	if (request.method=="POST"):
-		userinfo = [request.form['username'], sha256_crypt.hash(request.form['password']), request.form['fact']]
+		conn = sqlite3.connect('database.db')
+		cur = conn.cursor()
+		userinfo = [request.form['username'], request.form['password'], request.form['fact']]
 		cur.execute("INSERT INTO users VALUES (?,?,?)", (userinfo))
+		conn.commit()
+		conn.close()
+		return 'Sent.'
 	else:
-		return render_template("make.html")
-
-#might not work, delete this method if needed
-"""
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
-"""
-	
-conn.commit()
-conn.close()
+		return render_template("makeuser.html")
