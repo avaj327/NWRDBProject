616751951419c7b79ace0f9cb3a8cbc85b9c7bad
tjohnson327 <35973904+tjohnson327@users.noreply.github.com>
2018-11-29 07:48:56 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -11,6 +11,12 @@ import sqlite3
 from passlib.hash import sha256_crypt
 app = Flask(__name__)
 
+templates = {
+	"login": "nwr form.html",
+	"makeuser": "makeuser.html",
+	"user": "user.html"
+}
+
 app.secret_key = "53Da__de39^^w32$5)*8"
 
 @app.route('/')
@@ -21,7 +27,7 @@ def index():
 def hello():
 	return 'Hello, World'
 
-@app.route('/user')
+@app.route('/user/')
 def viewUser():
 	if ('user' in session):
 		user = session['user']
@@ -29,7 +35,7 @@ def viewUser():
 		password = user[1]
 		fact = user[2]
 		
-		return render_template('user.html', username=username,password=password,fact=fact)
+		return render_template(templates[user], username=username,password=password,fact=fact)
 	else:
 		return 'Please sign in.'
 
@@ -50,17 +56,32 @@ def login():
 				user = row
 				
 		if user == None:
-			return 'Invalid Username'
+			return render_template(templates[login], incorrect=True)
 			
 		if (sha256_crypt.verify(password, user[1]) == True):
 			session['user'] = user
 		else:
-			return 'Invalid Password'
+			return render_template(templates[login], incorrect=True)
 			
 		return redirect('/user')
 	else:
-		return render_template("form.html")
+		return render_template(templates[login], incorrect=False)
+
+@app.route('/userlist')
+def userlist():
+	conn = sqlite3.connect('database.db')
+	cur = conn.cursor()
+	users = cur.execute("SELECT * FROM users")
 	
+	names=[]
+	passwords=[]
+	facts=[]
+	for row in users:
+		names.append(row[0])
+		passwords.append(row[1])
+		facts.append(row[2])
+		
+	return render_template('userlist.html', names=names, passwords=passwords, facts=facts)
 
 @app.route('/makeuser', methods=["POST", "GET"])
 def makeuser():
