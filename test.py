01bf27779bab3a03b96f59dac9da330afa33dcc5
Pergsy <45045697+Pergsy@users.noreply.github.com>
2018-12-19 14:08:59 -0500

    Update test.py

--- test.py
+++ test.py
@@ -31,7 +31,7 @@ def hello():
 @app.route('/clublist/')
 def viewClubList():
     if ('user' in session):
-        return templates[clublist]
+        return templates[clubList]
     else:
         return redirect(login)
 
