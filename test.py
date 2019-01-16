bca2192b29a18e5db45f7879387b1ec2302ec4a0
Pergsy <45045697+Pergsy@users.noreply.github.com>
2019-01-16 11:54:08 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -63,97 +63,97 @@ def clublist():
 def viewUser():
     if ('user' in session):
         user = session['user']
-            username = user[0]
-                adminLevel = user[2]
-                rawMemberships = user[3].split(",")
-                rawAdvisories = user[4].split(",")
+        username = user[0]
+        adminLevel = user[2]
+        rawMemberships = user[3].split(",")
+        rawAdvisories = user[4].split(",")
                 
-                print ("RAW: " + str(rawMemberships))
+        print ("RAW: " + str(rawMemberships))
                 
-                memberships_ = []
-                advisories_ = []
+        memberships_ = []
+        advisories_ = []
                 
-                memberships = []
-                advisories = []
+        memberships = []
+        advisories = []
                 
-                membershipClubs = []
+        membershipClubs = []
                 
-                #strip unnecessary characters for memberships
-                for each in rawMemberships:
-                    i = 0
-                        length = len(each)
-                        while i < length:
-                            print (each[i])
-                                if each[i] == " ":
-                                    each = each[:i] + each[i+1:]
-                                if each[i] == "[":
-                                    each = each[:i] + each[i+1:]
-                                if each[i] == "]":
-                                    each = each[:i] + each[i+1:]
-                                if each[i] == "'":
-                                    each = each[:i] + each[i+1:]
-                                i += 1
-                                        length = len(each)
-                        each = each[:len(each)]
-                    memberships_.append(each)
+        #strip unnecessary characters for memberships
+        for each in rawMemberships:
+            i = 0
+            length = len(each)
+            while i < length:
+                print (each[i])
+                if each[i] == " ":
+                    each = each[:i] + each[i+1:]
+                if each[i] == "[":
+                    each = each[:i] + each[i+1:]
+                if each[i] == "]":
+                    each = each[:i] + each[i+1:]
+                if each[i] == "'":
+                    each = each[:i] + each[i+1:]
+                i += 1
+                length = len(each)
+            each = each[:len(each)]
+            memberships_.append(each)
         
-            #STUPID JANK FIX FOR END OF ARRAY ']'
-            last = memberships_[len(memberships_)-1]
-                lastLen = len(last)-1
-                if last[lastLen] == "]":
-                    last = last[:lastLen]
+        #STUPID JANK FIX FOR END OF ARRAY ']'
+        last = memberships_[len(memberships_)-1]
+        lastLen = len(last)-1
+        if last[lastLen] == "]":
+            last = last[:lastLen]
             
-                memberships_.pop(len(rawMemberships)-1)
-                memberships_.append(last)
+        memberships_.pop(len(rawMemberships)-1)
+        memberships_.append(last)
                 
-                last = rawMemberships[len(rawMemberships)-1]
-                lastLen = len(last)-1
+        last = rawMemberships[len(rawMemberships)-1]
+        lastLen = len(last)-1
                 
-                #replace '_' with spaces for memberships
-                for each in memberships_:
-                    for i in range(len(each)-1):
-                        if i < len(each):
-                            if each[i] == "_":
-                                each = each[:i] + " " + each[i+1:]
-                        each = each[:len(each)]
-                    memberships.append(each)
+        #replace '_' with spaces for memberships
+        for each in memberships_:
+            for i in range(len(each)-1):
+                if i < len(each):
+                    if each[i] == "_":
+                        each = each[:i] + " " + each[i+1:]
+            each = each[:len(each)]
+            memberships.append(each)
                 
-                #strip unnecessary characters for advisories
-                for each in rawAdvisories:
-                    i = 0
-                        length = len(each)-1
-                        while i <= length:
-                            print (each[i])
-                                if each[i] == " ":
-                                    each = each[:i] + each[i+1:]
-                                if each[i] == "[":
-                                    each = each[:i] + each[i+1:]
-                                if each[i] == "]":
-                                    each = each[:i] + each[i+1:]
-                                if each[i] == "'":
-                                    each = each[:i] + each[i+1:]
-                                i += 1
-                                        length = len(each)-1
-                        each = each[:len(each)]
-                    advisories_.append(each)
+        #strip unnecessary characters for advisories
+        for each in rawAdvisories:
+            i = 0
+            length = len(each)-1
+            while i <= length:
+                print (each[i])
+                if each[i] == " ":
+                    each = each[:i] + each[i+1:]
+                if each[i] == "[":
+                    each = each[:i] + each[i+1:]
+                if each[i] == "]":
+                    each = each[:i] + each[i+1:]
+                if each[i] == "'":
+                    each = each[:i] + each[i+1:]
+                i += 1
+                length = len(each)-1
+            each = each[:len(each)]
+            advisories_.append(each)
     
-    #replace '_' with spaces for advisories
-    for each in advisories_:
-        for i in range(len(each)-1):
-            if i < len(each):
-                if each[i] == "_":
-                    each = each[:i] + " " + each[i+1:]
-                        each = each[:len(each)]
-                        advisories.append(each)
+        #replace '_' with spaces for advisories
+        for each in advisories_:
+            for i in range(len(each)-1):
+                if i < len(each):
+                    if each[i] == "_":
+                        each = each[:i] + " " + each[i+1:]
+            each = each[:len(each)]
+            advisories.append(each)
             
-                #create membership club array from membership string array
-                for each in memberships:
-                    club = clubs(each)
-                        membershipClubs.append(club)
+        #create membership club array from membership string array
+        for each in memberships:
+            club = clubs(each)
+            membershipClubs.append(club)
                 
-                            return render_template(templates["user"], username=username,adminLevel=adminLevel,memberships=membershipClubs,advisories=advisories)
-                        else:
-                            return redirect('/login')
+        return render_template(templates["user"], username=username,adminLevel=adminLevel,memberships=membershipClubs,advisories=advisories)
+    else:
+        return redirect('/login')
 
 @app.route('/html')
 def html():
@@ -163,43 +163,43 @@ def html():
 def login():
     if (request.method=="POST"):
         conn = sqlite3.connect('database.db')
-            cur = conn.cursor()
-                username = request.form['username']
-                password = request.form['password']
-                user = None
-                for row in cur.execute("SELECT * FROM users"):
-                    if row[0] == username:
-                        user = row
+        cur = conn.cursor()
+        username = request.form['username']
+        password = request.form['password']
+        user = None
+        for row in cur.execute("SELECT * FROM users"):
+            if row[0] == username:
+            user = row
                 
-                if user == None:
-                    return render_template(templates["login"], incorrect=True)
+        if user == None:
+            return render_template(templates["login"], incorrect=True)
                 
-                if (sha256_crypt.verify(password, user[1]) == True):
-                    session['user'] = user
-                else:
-                    return render_template(templates["login"], incorrect=True)
-                
-                        return redirect('/user')
+        if (sha256_crypt.verify(password, user[1]) == True):
+            session['user'] = user
         else:
-            return render_template(templates["login"], incorrect=False)
+            return render_template(templates["login"], incorrect=True)
+                
+        return redirect('/user')
+    else:
+        return render_template(templates["login"], incorrect=False)
 
 @app.route('/userlist')
 def userlist():
     conn = sqlite3.connect('database.db')
-        cur = conn.cursor()
-        users = cur.execute("SELECT * FROM users")
+    cur = conn.cursor()
+    users = cur.execute("SELECT * FROM users")
         
-        names=[]
-        passwords=[]
-        levels=[]
-        memberships=[]
-        advisories=[]
-        for row in users:
-            names.append(row[0])
-                passwords.append(row[1])
-                levels.append(row[2])
-                memberships.append(row[3])
-                advisories.append(row[4])
+    names=[]
+    passwords=[]
+    levels=[]
+    memberships=[]
+    advisories=[]
+    for row in users:
+        names.append(row[0])
+            passwords.append(row[1])
+            levels.append(row[2])
+            memberships.append(row[3])
+            advisories.append(row[4])
         
     return render_template('userlist.html', names=names, passwords=passwords, levels=levels, memberships=memberships, advisories=advisories)
 
@@ -217,3 +217,8 @@ def makeuser():
         return "Sent."
     else:
         return render_template(templates["makeuser"])
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/login')
