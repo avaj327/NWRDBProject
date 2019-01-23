5d8548460020a92e8df6b5528dab38cf0d5f4947
Pergsy <45045697+Pergsy@users.noreply.github.com>
2019-01-23 12:17:43 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -51,7 +51,7 @@ def viewDataEntry():
     if (request.method=="POST"):
         conn = sqlite3.connect('database.db')
         cur = conn.cursor()
-        entry = [user[0] + pointer + request.form[activity], int(request.form[activity]), request.form[hours], int(request.form[approved])]
+        entry = [user[0] + pointer + 'PLACEHOLDER', request.form[activity], int(request.form[hours]), int(request.form[approved])]
         cur.execute("INSERT INTO userEntries VALUES(?,?,?,?)", entry)
         conn.commit()
         conn.close()
