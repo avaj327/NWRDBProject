3a6bdeb62a953a891dbe8f5e986d7c482cda2877
tjohnson327 <35973904+tjohnson327@users.noreply.github.com>
2019-04-02 11:53:46 -0400

    Add files via upload

--- clubs.py
+++ clubs.py
@@ -8,8 +8,6 @@ Created on Fri Dec 14 12:30:03 2018
 
 class clubs():
 	global infoList
-	global entries
-	entries = []
 	infoList  = {
 		"None": "You have no clubs! Go to the club list to join some!",
 		"Interact": "Description for Interact",
@@ -26,6 +24,8 @@ class clubs():
 	def __init__(self, name):
 		global infoList
 		self.name = name
+		self.entries = []
+		self.totalHours = 0
 		if name in infoList:
 			self.desc = infoList[name]
 		else:
@@ -34,18 +34,9 @@ class clubs():
 	def __str__(self):
 		return (self.name + ": " + self.desc)
 
-	def getEntries(self):
-		global entries
-		return entries
-
-	def getTotalHours(self):
-		for entry in entries:
-			total += int(entry[2])
-		return total
-
 	def addEntry(self, entry):
-		global entries
-		entries.append(entry)
+		self.entries.append(entry)
+		totalHours += entry[2]
 
 	#will return a list of all of the registered clubs as club objects
 	@staticmethod
