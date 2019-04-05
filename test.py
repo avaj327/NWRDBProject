c59b1adbcfa3c97d93b618dd832bee9018e5eb3a
Pergsy <45045697+Pergsy@users.noreply.github.com>
2019-04-05 13:22:41 -0400

    Update test.py

--- test.py
+++ test.py
@@ -259,7 +259,7 @@ def makeuser():
 	if (request.method=="POST"):
 		conn = sqlite3.connect('database.db')
 		cur = conn.cursor()
-		userinfo = [request.form['username'], sha256_crypt.hash(request.form['password']), int(request.form['adminLevel']), str(request.form.getlist('memberships')), "none"]
+		userinfo = [request.form['username'], sha256_crypt.hash(request.form['password']), int(request.form['adminLevel']), str(request.form.getlist('memberships')), str(request.form.getlist('advisories'))]
 		cur.execute("INSERT INTO users VALUES (?,?,?,?,?)", userinfo)
 		#for userClub in request.form.getlist('memberships'):
 		#    fixName(userClub)
