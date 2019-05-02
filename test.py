03e30aaf46e4d973e01dcbce59e24b7473a791da
tjohnson327 <35973904+tjohnson327@users.noreply.github.com>
2019-05-02 14:09:08 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -9,7 +9,8 @@ from flask import Flask, request, render_template, session, redirect, url_for
 import sqlite3
 from passlib.hash import sha256_crypt
 from clubs import clubs
-#TODO https://stackoverflow.com/questions/21498694/flask-get-current-route/21498747
+import yagmail
+
 app = Flask(__name__)
 
 templates = {
@@ -41,6 +42,19 @@ def fixName (clubName):
 		if char == '〰':
 			char = '-'
 
+def refreshEntries():
+	conn = sqlite3.connect('database.db')
+	cur = conn.cursor()
+	username = session['user'][0]
+	entries = []
+	for i,row in enumerate(cur.execute("SELECT * FROM userEntries")):
+		if row[0] == username:
+			entries.append(row)
+	session['entries'] = entries
+
+	conn.commit()
+	conn.close()
+
 @app.route('/')
 def index():
 	return 'Index Page'
@@ -49,7 +63,7 @@ def index():
 def hello():
 	return 'Hello, World'
 
-@app.route('/dataentry', methods=["POST", "GET"])
+@app.route('/dataentry/', methods=["POST", "GET"])
 def dataEntry():
 	if (request.method=="POST"):
 		user = session['user']
@@ -67,7 +81,7 @@ def dataEntry():
 	else:
 		return redirect("/login")
 
-@app.route('/entrylist')
+@app.route('/entrylist/')
 def viewEntries():
 	conn = sqlite3.connect('database.db')
 	cur = conn.cursor()
@@ -103,105 +117,29 @@ def clublist():
 
 @app.route('/user', methods=["POST","GET"])
 def viewUser():
-	if ('user' in session):
+	if (request.method=="POST"):
 		user = session['user']
-		username = user[0] #SEAN ES MUY STUPIDren Club DatabaO
+		conn = sqlite3.connect('database.db')
+		cur = conn.cursor()
+		entry = [session['user'][0], request.form['club'], request.form['activity'], int(request.form['hours']), 0]
+		cur.execute("INSERT INTO userEntries VALUES(?,?,?,?,?)", entry)
+		conn.commit()
+		conn.close()
+
+		refreshEntries()
+		return redirect("/user")
+	elif ('user' in session):
+		user = session['user']
+		username = user[0] #SEAN ES MUY STUPIDO
 		adminLevel = user[2]
-		rawMemberships = user[3].split(",")
-		rawAdvisories = user[4].split(",")
-		entries = session['entries']
+		rawMemberships = user[3]
 
-		print (rawMemberships)
-		print(rawAdvisories)
+		memberships = fixArray(rawMemberships)
 
-		memberships_ = []
-		advisories_ = []
+		refreshEntries()
 
-		memberships = []
-		advisories = []
+		return render_template(templates["user"], username=username,adminlevel=adminLevel,memberships=memberships)
 
-		membershipClubs = []
-
-		if rawMemberships != ['[]']:
-			#strip unnecessary characters for memberships
-			for each in rawMemberships:
-				i = 0
-				length = len(each)
-				while i < length:
-					if each[i] == " ":
-						each = each[:i] + each[i+1:]
-					if each[i] == "[":
-						each = each[:i] + each[i+1:]
-					if each[i] == "]":
-						each = each[:i] + each[i+1:]
-					if each[i] == "'":
-						each = each[:i] + each[i+1:]
-					i += 1
-					length = len(each)
-				each = each[:len(each)]
-				memberships_.append(each)
-
-			#STUPID JANK FIX FOR END OF ARRAY ']'
-			last = memberships_[len(memberships_)-1]
-			lastLen = len(last)-1
-			if last[lastLen] == "]":
-				last = last[:lastLen]
-
-			memberships_.pop(len(rawMemberships)-1)
-			memberships_.append(last)
-
-			last = rawMemberships[len(rawMemberships)-1]
-			lastLen = len(last)-1
-
-			#replace '_' with spaces for memberships
-			for each in memberships_:
-				for i in range(len(each)-1):
-					if i < len(each):
-						if each[i] == "_":
-							each = each[:i] + " " + each[i+1:]
-				each = each[:len(each)]
-				memberships.append(each)
-		else:
-			memberships.append("None")
-
-		if rawAdvisories != ['[]']:
-			#strip unnecessary characters for advisories
-			for each in rawAdvisories:
-				i = 0
-				length = len(each)-1
-				while i <= length:
-					if each[i] == " ":
-						each = each[:i] + each[i+1:]
-					if each[i] == "[":
-						each = each[:i] + each[i+1:]
-					if each[i] == "]":
-						each = each[:i] + each[i+1:]
-					if each[i] == "'":
-						each = each[:i] + each[i+1:]
-					i += 1
-					length = len(each)-1
-				each = each[:len(each)]
-				advisories_.append(each)
-
-			#replace '_' with spaces for advisories
-			for each in advisories_:
-				for i in range(len(each)-1):
-					if i < len(each):
-						if each[i] == "_":
-							each = each[:i] + " " + each[i+1:]
-				each = each[:len(each)]
-				advisories.append(each)
-
-		#create membership club array from membership string array
-		for each in memberships:
-			club = clubs(each)
-			for entry in entries:
-				if club.name == entry[1]:
-					club.addEntry(entry)
-
-			membershipClubs.append(club)
-
-		return render_template(templates["user"], username=username,adminlevel=adminLevel,memberships=membershipClubs,advisories=advisories)
 	else:
 		return redirect('/login')
 
@@ -210,7 +148,9 @@ def viewAdmin():
 	if 'user' in session:
 		user = session['user']
 		if user[2] != 0:
-			return render_template(templates['admin'],adminlevel=session['user'][2],username=session['user'][0])
+			advisories = fixArray(user[4])
+
+			return render_template(templates['admin'],adminlevel=session['user'][2],username=session['user'][0],advisories=advisories)
 		else:
 			return redirect('/user')
 	else:
@@ -219,6 +159,70 @@ def viewAdmin():
 @app.route('/html')
 def html():
 	return render_template('test.html', name='Sean')
+
+def fixArray(array):
+	user = session['user']
+	rawArray = user[3].split(",")
+	entries = session['entries']
+
+	array_ = []
+
+	array = []
+
+	arrayClubs = []
+
+	if rawArray != ['[]']:
+		#strip unnecessary characters for memberships
+		for each in rawArray:
+			i = 0
+			length = len(each)
+			while i < length:
+				if each[i] == " ":
+					each = each[:i] + each[i+1:]
+				if each[i] == "[":
+					each = each[:i] + each[i+1:]
+				if each[i] == "]":
+					each = each[:i] + each[i+1:]
+				if each[i] == "'":
+					each = each[:i] + each[i+1:]
+				i += 1
+				length = len(each)
+			each = each[:len(each)]
+			array_.append(each)
+
+		#STUPID JANK FIX FOR END OF ARRAY ']'
+		last = array_[len(array_)-1]
+		lastLen = len(last)-1
+		if last[lastLen] == "]":
+			last = last[:lastLen]
+
+		array_.pop(len(rawArray)-1)
+		array_.append(last)
+
+		last = rawArray[len(rawArray)-1]
+		lastLen = len(last)-1
+
+		#replace '_' with spaces for memberships
+		for each in array_:
+			for i in range(len(each)-1):
+				if i < len(each):
+					if each[i] == "_":
+						each = each[:i] + " " + each[i+1:]
+			each = each[:len(each)]
+			array.append(each)
+	else:
+		array.append("None")
+
+	#create membership club array from membership string array
+	for each in array:
+		club = clubs(each)
+		for entry in entries:
+			if club.name == entry[1]:
+				club.addEntry(entry)
+
+		arrayClubs.append(club)
+
+	return arrayClubs
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
@@ -234,15 +238,11 @@ def login():
 
 		if user == None:
 			return render_template(templates["login"], incorrect=True)
-		else:
-			entries = []
-			for i,row in enumerate(cur.execute("SELECT * FROM userEntries")):
-				if row[0] == username:
-					entries.append(row)
+
 
 		if (sha256_crypt.verify(password, user[1]) == True):
 			session['user'] = user
-			session['entries'] = entries
+			refreshEntries()
 		else:
 			return render_template(templates["login"], incorrect=True)
 
